1166a2a736f4a56411ca96d457b3c4e639694bd7
Berzan <125147438+realwestman@users.noreply.github.com>
2026-03-01 20:29:54 +0100

    Add files via upload

---
+++ my-pwa/sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'min-pwa-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
